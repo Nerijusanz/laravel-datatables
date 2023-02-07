118484062e96566b5c922a9e7d55359b0d36c209
Nerijus <nerijusanz@gmail.com>
2023-02-07 11:43:57 +0200

    fix route customer

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'CustomerController@index')->name('simple');
+Route::get('/',[CustomerController::class, 'index'])->name('simple');
+
